4d156a172050c96dba75f757250f944d1f437818
Isaiah Mann <isaiahbmann@gmail.com>
2019-09-08 11:41:52 -0700

    Make current dir func in test more rubst

--- balanced_bst/test.sh
+++ balanced_bst/test.sh
@@ -1,5 +1,17 @@
 #! /bin/sh
 
-DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+# Source: https://www.ostricher.com/2014/10/the-right-way-to-get-the-directory-of-a-bash-script/
+get_script_dir () {
+     SOURCE="${BASH_SOURCE[0]}"
+     # While $SOURCE is a symlink, resolve it
+     while [ -h "$SOURCE" ]; do
+          DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+          SOURCE="$( readlink "$SOURCE" )"
+          # If $SOURCE was a relative symlink (so no "/" as prefix, need to resolve it relative to the symlink base directory
+          [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+     done
+     DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+     echo "$DIR"
+}
 
-python3 ${DIR}/test_balanced_bst.py
+python3 $(get_script_dir)/test_balanced_bst.py
