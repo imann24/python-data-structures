9fa7e5f58c6636a51e9559a24dddc4c715cfa8f3
Isaiah Mann <isaiahbmann@gmail.com>
2019-09-08 12:06:35 -0700

    [CI] Refactor header print to util func

---
+++ util/print_header.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+title=$1
+title_length=${#title}
+
+function print_spacer () {
+    printf "*%.0s" $(seq 1 $title_length)
+    echo
+}
+
+print_spacer
+print_spacer
+echo $title
+print_spacer
+print_spacer
